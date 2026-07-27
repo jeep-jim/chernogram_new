241cb714dc5835f9bd77a33546e10347aa0abc96
jeep-jim <yokkey@ya.ru>
2026-07-27 11:09:58 +0700

    Объединить обновления Android и Windows

---
+++ lib/app_update_service.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+import 'update_service.dart';
+import 'windows_update_service.dart';
+
+class ChernogramAppUpdater {
+  static Future<void> checkAndPrompt(
+    BuildContext context, {
+    required bool ru,
+    bool manual = false,
+  }) async {
+    if (Platform.isWindows) {
+      await ChernogramWindowsUpdater.checkAndPrompt(
+        context,
+        ru: ru,
+        manual: manual,
+      );
+      return;
+    }
+
+    await ChernogramUpdater.checkAndPrompt(
+      context,
+      ru: ru,
+      manual: manual,
+    );
+  }
+}
