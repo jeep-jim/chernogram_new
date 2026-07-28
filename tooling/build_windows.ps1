331070631b6e5e5a2b509cae29a40115412b5ac3
jeep-jim <yokkey@ya.ru>
2026-07-28 10:38:02 +0700

    Подключить быстрый патч 0.16.5 к Windows

--- tooling/build_windows.ps1
+++ tooling/build_windows.ps1
@@ -54,6 +54,9 @@ try {
   Invoke-LoggedCommand `
     -Name 'FIX 0.16.4 FEEDBACK' `
     -Command 'python tooling/fix_v0164_feedback.py'
+  Invoke-LoggedCommand `
+    -Name 'FIX 0.16.5 CALLS AND PRESENCE' `
+    -Command 'python tooling/fix_v0165_reliability.py'
   Invoke-LoggedCommand -Name 'PUB GET' -Command 'flutter pub get'
   Invoke-LoggedCommand -Name 'FORMAT' -Command 'dart format lib'
   Invoke-LoggedCommand `
