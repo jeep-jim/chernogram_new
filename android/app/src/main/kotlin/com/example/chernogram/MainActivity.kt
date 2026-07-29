3287dec6480ee1b63198ae8afa47cdb481e2d850
jeep-jim <yokkey@ya.ru>
2026-07-29 16:26:49 +0700

    Подключить Android биометрию без потери звонков

--- android/app/src/main/kotlin/com/example/chernogram/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/chernogram/MainActivity.kt
@@ -6,11 +6,11 @@ import android.media.RingtoneManager
 import android.os.Build
 import android.os.VibrationEffect
 import android.os.Vibrator
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity() {
+class MainActivity : FlutterFragmentActivity() {
     private val channelName = "chernogram/sound"
     private var incomingRingtone: Ringtone? = null
 
